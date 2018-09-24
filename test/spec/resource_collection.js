b87f74205eb7a5bb03d895fafbba2e68199e1387
Joseph Abell <joseph.abell@piksel.com>
2018-09-24 12:09:53 +0100

    test through relationships on resource_collection

--- test/spec/resource_collection.js
+++ test/spec/resource_collection.js
@@ -5,17 +5,22 @@ import Transport from '../../lib/transport';
 import mediaItemFixture from '../fixtures/media-items.json';
 import lastMediaItemFixture from '../fixtures/media-items-12.json';
 import metadataDescriptorFixture from '../fixtures/metadata-descriptor.json';
+import creditsWithThroughFixture from '../fixtures/credits-with-through.json';
 
 describe('ResourceCollection', () => {
-  const mockDescriptor = Object.assign(metadataDescriptorFixture.resourcefuls.contents, {
-    location: 'http://localhost/metadata',
-    tenant: 'test',
-    pluralName: 'contents'
-  });
-  const resourcefulEndpoint = new ResourcefulEndpoint(new Transport(), mockDescriptor);
+  let mockDescriptor;
+  let resourcefulEndpoint;
   let resourceCollection;
 
   beforeEach(() => {
+    mockDescriptor = Object.assign(metadataDescriptorFixture.resourcefuls.contents, {
+      location: 'http://localhost/metadata',
+      tenant: 'test',
+      pluralName: 'contents'
+    });
+
+    resourcefulEndpoint = new ResourcefulEndpoint(new Transport(), mockDescriptor);
+
     resourceCollection = new ResourceCollection(mediaItemFixture);
   });
 
@@ -172,6 +177,85 @@ describe('ResourceCollection', () => {
       const result = resourceCollection.setData(rawData);
       expect(result.rawData.linked).toEqual({});
       expect(result.collection).toEqual([]);
+    });
+
+    describe('through relationships', () => {
+      let rawData;
+      beforeEach(() => {
+        mockDescriptor = Object.assign(metadataDescriptorFixture.resourcefuls.credits, {
+          location: 'http://localhost/metadata',
+          tenant: 'test',
+          pluralName: 'credits'
+        });
+        resourcefulEndpoint = new ResourcefulEndpoint(new Transport(), mockDescriptor);
+        resourceCollection = new ResourceCollection(
+          mediaItemFixture,
+          null,
+          resourcefulEndpoint
+        );
+        rawData = creditsWithThroughFixture;
+      });
+
+      it('has a resourcefulEndpoint that has a relationship that has a through relationship', () => {
+        const relationship = resourcefulEndpoint.resourceful.relationships['relatedMaterials.assets'];
+        expect(relationship).toBeDefined();
+        expect(relationship.through).toBeDefined();
+      });
+
+      it('has rawData', () => {
+        expect(rawData).toBeDefined();
+      });
+
+      it('has a collection', () => {
+        const result = resourceCollection.setData(rawData);
+        expect(result && result.collection).toBeDefined();
+      });
+
+      it('has an item in collection that has values in relatedMaterialRefs', () => {
+        const result = resourceCollection.setData(rawData);
+        const { collection } = result;
+        const itemWithRelatedMaterialRefs = collection[0];
+
+        expect(
+          itemWithRelatedMaterialRefs &&
+          itemWithRelatedMaterialRefs.relatedMaterialRefs
+        ).toBeDefined();
+
+        expect(itemWithRelatedMaterialRefs.relatedMaterialRefs.length).toEqual(1);
+      });
+
+      describe('item with relatedMaterialRefs', () => {
+        let result;
+        let collection;
+        let itemWithRelatedMaterialRefs;
+
+        beforeEach(() => {
+          result = resourceCollection.setData(rawData);
+          collection = result && result.collection;
+          itemWithRelatedMaterialRefs = collection[0];
+        });
+
+        it('has linked data', () => {
+          expect(itemWithRelatedMaterialRefs.linked).toBeDefined();
+        });
+
+        it('has the same number of linked data the amount of relatedMaterialRefs', () => {
+          const refs = itemWithRelatedMaterialRefs.relatedMaterialRefs;
+          const linked = itemWithRelatedMaterialRefs.linked['relatedMaterials.assets'];
+          expect(refs.length).toEqual(linked.length);
+        });
+
+        it('has matching linked data and relatedMaterialRefs', () => {
+          const refs = itemWithRelatedMaterialRefs.relatedMaterialRefs;
+          const linked = itemWithRelatedMaterialRefs.linked['relatedMaterials.assets'];
+
+          linked
+            .map(l => l.contentRef)
+            .forEach((l) => {
+              expect(refs.includes(l));
+            });
+        });
+      });
     });
   });
 
