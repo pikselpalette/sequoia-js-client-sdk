7aaca1bc56ab547be00f4b0918c9aa69da65d997
Mike Hutchinson <michael.hutchinson@piksel.com>
2018-10-15 10:51:40 +0100

    Remove stryker babel transpiler. Remove stryker from CI run.

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,39 +1,24 @@
-const getMutatePattern = () => {
-  for (let i = 0; i < process.argv.length; i++) {
-    const argument = process.argv[i].split('=');
-
-    if (argument[0] === '--file') {
-      return [
-        argument.slice(1).join('='),
-        'lib/**/*.js'
-      ];
-    }
-  }
-
-  return ['lib/**/*.js'];
-};
-
 module.exports = (config) => {
   config.set({
     files: [
       'test/**/*.json',
       'test/**/*.js',
-      ...getMutatePattern()
+      'lib/**/*.js'
+    ],
+    mutate: [
+      'lib/**/*.js'
     ],
     timeoutFactor: 10,
-    timeoutMs: 60000,
+    timeoutMS: 60000,
     testRunner: 'jest',
     mutator: 'javascript',
     coverageAnalysis: 'off',
     plugins: [
       'stryker-jest-runner',
       'stryker-html-reporter',
-      'stryker-babel-transpiler',
       'stryker-javascript-mutator'
     ],
-    babelrcFile: '.babelrc',
-    transpilers: ['babel'],
-    reporter: ['dots', 'clear-text', 'html'],
+    reporters: ['progress', 'dots', 'clear-text', 'html'],
     htmlReporter: {
       baseDir: 'test/results/mutation/html'
     }
